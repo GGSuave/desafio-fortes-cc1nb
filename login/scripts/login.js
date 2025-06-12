febe50f3e314eff61f3887d5615cd72da159c655
Guilherme Suave <guilherme.suave@el.com.br>
2025-06-12 17:22:32 -0300

    feat(login): FInalização da tela de login

--- login/scripts/login.js
+++ login/scripts/login.js
@@ -11,12 +11,72 @@ const dados = [
 	},
 ];
 
+function verificarErro(email, senha) {
+	const emailElemento = document.getElementById("email");
+	const senhaElemento = document.getElementById("senha");
+
+	let erroEmail;
+	let erroSenha;
+
+	if (email == "" || email == undefined || email == null) {
+		let erroEmailElemento = document.getElementById("erro-email");
+
+		erroEmailElemento.classList.remove("none");
+		emailElemento.classList.remove(".input-sem-erro");
+		emailElemento.classList.add("input-erro");
+		erroEmail = true;
+	}
+
+	if (senha == "" || senha == undefined || senha == null) {
+		let erroSenhaElemento = document.getElementById("erro-senha");
+
+		erroSenhaElemento.classList.remove("none");
+		senhaElemento.classList.remove(".input-sem-erro");
+		senhaElemento.classList.add("input-erro");
+		erroSenha = true;
+	}
+
+	if (erroEmail || erroSenha) return true;
+
+	return false;
+}
+
+function removerErroEmail() {
+	const emailElemento = document.getElementById("email");
+	let erroEmailElemento = document.getElementById("erro-email");
+
+	erroEmailElemento.classList.add("none");
+
+	emailElemento.classList.remove("input-erro");
+	emailElemento.classList.add("input-sem-erro");
+}
+
+function removerErroSenha() {
+	const senhaElemento = document.getElementById("senha");
+
+	let erroSenhaElemento = document.getElementById("erro-senha");
+
+	erroSenhaElemento.classList.add("none");
+
+	senhaElemento.classList.remove("input-erro");
+	senhaElemento.classList.add("input-sem-erro");
+}
+
 function logar() {
+	const erroConta = document.getElementById("erro-conta");
+
+	if (!erroConta.classList.contains("none")) {
+		erroConta.classList.add("none");
+	}
+
 	const email = document.getElementById("email").value;
 	const senha = document.getElementById("senha").value;
 
+	let preencheu = verificarErro(email, senha);
+
+	if (preencheu) return;
+
 	let adm = verificarCadastro(email, senha, 0);
-	debugger;
 
 	if (adm.verificado) {
 		window.open("../", "_self");
@@ -33,11 +93,13 @@ function logar() {
 		localStorage.setItem("user", cooperativa.user);
 		return;
 	}
+
+	erroConta.classList.remove("none");
 }
 
 function verificarCadastro(email, senha, user) {
 	if (email === dados[user].email && senha == dados[user].senha) {
-		return {verificado: true, user: dados[user]};
+		return { verificado: true, user: dados[user] };
 	}
-	return {verificado: false};
+	return { verificado: false };
 }
