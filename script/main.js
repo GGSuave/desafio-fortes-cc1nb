5bd4bf9bff88cc574778cd1af784d391b70f0f84
Guilherme Suave <gsuave179@gmail.com>
2025-06-13 17:55:25 -0300

    teste

--- script/main.js
+++ script/main.js
@@ -4,6 +4,7 @@ let user;
 if (logado == null) {
 	window.location.href = "./login/";
 } else {
+	alert("logado");
 	user = JSON.parse(localStorage.getItem("user"));
 
 	window.open(`/${user.tipo}/`, "_self");
